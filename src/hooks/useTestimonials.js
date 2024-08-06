c36d2acbea106bc93c34e5d2456b9403e8ae9f73
Katerina Matsuleva <katerina.matsuleva@eveliko.com>
2024-08-06 19:19:43 +0300

    Clean Up

--- src/hooks/useTestimonials.js
+++
@@ -1,8 +0,0 @@
-import testimonialsAPI from "../api/testimonials-api";
-
-export default function useCreateTestimonial() {
-  const handleCreate = (barberId, review) =>
-    testimonialsAPI.createTestimonial(barberId, review);
-
-  return handleCreate;
-}
