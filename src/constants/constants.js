5d42afe446f722203815f0be5f56e53dc6645704
Katerina Matsuleva <katerina.matsuleva@eveliko.com>
2024-08-06 15:12:50 +0300

    Added constants

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+export const NAVIGATION = [
+  { title: "Home", url: "/" },
+  { title: "Services", url: "/services" },
+  { title: "Barbers", url: "/barbers" },
+  { title: "Contacts", url: "/contacts" },
+];
+
+export const AUTHENTICATED_NAVIGATION = [
+  { title: "Dashboard", url: "/dashboard" },
+  { title: "Log out", url: "/logout" },
+];
+
+export const UNAUTHENTICATED_NAVIGATION = [
+  { title: "Login", url: "/login" },
+  { title: "Register", url: "/register" },
+];
